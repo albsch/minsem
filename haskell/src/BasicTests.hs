8fa4b26c246075921ee5e60efbd3f57a44018900
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2024-05-14 11:36:53 +0200

    one more test

--- haskell/src/BasicTests.hs
+++ haskell/src/BasicTests.hs
@@ -59,6 +59,7 @@ test_basicMinsem = do
     printRepr M.impl TyBottom
     printRepr M.impl (TyAtom True)
     printRepr M.impl (TyAtom False)
+    printRepr M.impl (TyProd (TyAtom False) (TyAtom True))
     printRepr M.impl (TyInter (TyAtom False) (TyNeg TyBottom))
     printRepr M.impl (TyUnion TyTop TyTop)
     printRepr M.impl (TyInter TyTop TyTop)
